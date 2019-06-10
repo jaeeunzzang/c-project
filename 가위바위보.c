22b6e75b4bf3ef772ee9ae2dc45ba44744b2d859
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-06-10 10:25:38 +0900

    Update 가위바위보.c

--- 가위바위보.c
+++ 가위바위보.c
@@ -27,39 +27,40 @@ void main()
       printf("컴퓨터는 %s를 냈습니다\n",(computer==1 ? "가위" : computer==2 ? "바위" : "보"));
       printf("유저는 %s를 냈습니다\n",(user==1 ? "가위" : user==2 ? "바위" : "보"));
       
-      if(computer==user)
-      {
-        printf("무승부입니다\n");
-        result[1]++;
-      }
-      else if(
-        (computer==1 && user==3)||
-        (computer==2 && user==1)||
-        (computer==3 && user==2))
-      {
-        printf("패배했습니다\n");
-        result[0]++;
-      }
-      else
-      {
-        printf("승리했습니다\n");
-      result[2]++;
-      }
+         if(computer==user)
+           {
+            printf("무승부입니다\n");
+            result[1]++;
+            }
+           else if(
+            (computer==1 && user==3)||
+            (computer==2 && user==1)||
+            (computer==3 && user==2))
+              {
+                 printf("패배했습니다\n");
+                 result[0]++;
+               }
+            else
+              {
+                printf("승리했습니다\n");
+                result[2]++;
+              }
      
-    }
+       }
     else
     {
-      if (user==0)
-      {
-        printf("%d전 %d승 %d무 %d패 입니다.\n" ,cnt,result[2],result[1],result[0]);
-        printf("게임을 종료합니다.\n");
+        if (user==0)
+           {
+               printf("%d전 %d승 %d무 %d패 입니다.\n" ,cnt,result[2],result[1],result[0]);
+               printf("게임을 종료합니다.\n");
         
-        break;
-      }
+                break;
+           }
       printf("1,2,3,0중에서 입력해주세요\n");
     }
     
   }
+  
   return 0;
 }
         
