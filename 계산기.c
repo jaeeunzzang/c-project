2ec011e76b48b2041f39e5cad3866a525bbd7e53
jaeeunzzang <50936779+jaeeunzzang@users.noreply.github.com>
2019-06-25 15:45:05 +0900

    Update 계산기.c

--- 계산기.c
+++ 계산기.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
 
+double add(double num1, double num2)
+{
+  return num1+num2;
+}
+
+double sub(double num1, double num2)
+{
+  return num1-num2;
+}
+
+double mul(double num1, double num2)
+{
+  return num1*num2;
+}
+
+double div(double num1, double num2)
+{
+  return num1/num2;
+}
+
+int main(void)
+{
+  double num1,num2;
+  
+  printf("첫번째숫자: ");
+  scanf("%.2f",&num1);
+  printf("두번째숫자: ");
+  scanf("%.2f",&num2);
+  
+  printf("%.2f + %.2f = %.2f\n",num1,num2,add(num1,num2));
+  printf("%.2f - %.2f = %.2f\n",num1,num2,sub(num1,num2));
+  printf("%.2f * %.2f = %.2f\n",num1,num2,mul(num1,num2));
+  printf("%.2f / %.2f = %.2f\n",num1,num2,div(num1,num2));
+  return 0;
+  
+}
